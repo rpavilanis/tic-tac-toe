c38a5ed84024642286d8fb084dee5391153cd6c6
Rachel <rachel.l.pavilanis@gmail.com>
2016-12-12 15:09:14 -0800

    added beginning of a board

--- src/game.js
+++ src/game.js
@@ -1,5 +1,25 @@
 //game.js
+// var prompt = require('prompt');
 
+//start the prompt
+// prompt.start();
+
+var Game = function() {
+  this.board.firstRow = ['*', '*', '*'];
+  this.board.secondRow = ['*', '*', '*'];
+  this.board.thirdRow = ['*', '*', '*'];
+
+  // var playerX = function(){
+  //   this.name = name;
+  // };
+  //
+  // var playerO = function(){};
+
+};
+
+
+// var game = new Game();
+// prompt.get(['playerX','playerO'], game);
 
 module.exports = Game;
 
