29747bfa108d27822a0cbf8e78cd5c016984e94e
Erin Bond <erin@erinebond.com>
2016-12-12 15:05:49 -0800

    First test to see if the game board first row works

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -3,7 +3,7 @@ describe('Game', function() {
   var testGame = Game;
   describe('board', function() {
     it('should display an empty board, array of arrays', function() {
-        expect(typeof testGame.board()).toEqual("array");
+        expect(typeof testGame.board.firstRow()).toEqual("array");
     });
   });
 });
